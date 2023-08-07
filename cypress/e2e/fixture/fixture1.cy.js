88ab99664673bf529c320cce86d8f622738d086d
Kento182 <128067001+Kento182@users.noreply.github.com>
2023-08-06 21:39:06 -0600

    cucumber

--- cypress/e2e/fixture/fixture1.cy.js
+++ cypress/e2e/fixture/fixture1.cy.js
@@ -3,7 +3,7 @@
 require('cypress-xpath')
 
 describe('Carga por Fixture', () =>{
-    let tiempo=1000
+    let tiempo=500
 
     before('Inicio',() =>{
         //cy.fixture('datos').then((data) => {
@@ -15,11 +15,25 @@ describe('Carga por Fixture', () =>{
     })
 
     it('Test uno: Cargando desde json', () => {
-        cy.Texto('#wsf-1-field-21',data.name,tiempo)
-        cy.Texto('#wsf-1-field-22',data.surname,tiempo)
-        cy.Texto('#wsf-1-field-23',data.email,tiempo)
-        cy.Texto('#wsf-1-field-24',data.phone,tiempo)
-        cy.Texto('#wsf-1-field-28',data.phone,tiempo)
-        cy.Click('#wsf-1-field-27',tiempo)
+        cy.fixture('MOCK_DATA').then(i => {
+            i.forEach(data => {
+                //const d_name=data.name
+                //const d_surname=data.surname
+                //const d_email=data.email
+                //const d_phone=data.phone
+                //const d_adress=data.adress        No es necesario
+
+                cy.Texto('#wsf-1-field-21',data.name,tiempo)
+                cy.Texto('#wsf-1-field-22',data.surname,tiempo)
+                cy.Texto('#wsf-1-field-23',data.email,tiempo)
+                cy.Texto('#wsf-1-field-24',data.phone,tiempo)
+                cy.Texto('#wsf-1-field-28',data.adress,tiempo)
+                cy.Click('#wsf-1-field-27',tiempo)
+
+                cy.visit('https://testingqarvn.com.es/datos-personales/')
+                cy.title().should('eq','Datos Personales | TestingQaRvn')
+                cy.wait(tiempo)
+            })
+        })        
     })
 })
